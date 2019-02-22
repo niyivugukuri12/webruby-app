2bbf01bfbcd81a82e07efd759c7fe0fc61ef07e5
jniyivugukuri <jniyivugukuri@yahoo.com>
2019-02-22 15:38:31 +0200

    firt commit

---
+++ db/migrate/20190212123251_create_resumes.rb
@@ -0,0 +1,14 @@
+class CreateResumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :resumes do |t|
+      t.string :title
+      t.string :category
+      t.string :released
+      t.string :year
+      t.string :price
+      t.string :attachment
+
+      t.timestamps
+    end
+  end
+end
